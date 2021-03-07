e2fc0453068600d7d82931a827e4cf98c4ecf7e9
radulfr-dev <cameron.raw89@gmail.com>
2021-03-07 14:54:53 +0100

    Included sass and jest

---
+++ src/__tests__/ordersController.test.js
@@ -0,0 +1,7 @@
+const { TestScheduler } = require("jest");
+
+describe("Orders Controller", () => {
+    test("Your mum should be a slag", () => {
+        expect("Your mum").toBe("a slag!");
+    });
+});
---
+++ src/__tests__/servicesController.test.js
@@ -0,0 +1,3 @@
+describe("Services Controller", () => {
+    
+});
